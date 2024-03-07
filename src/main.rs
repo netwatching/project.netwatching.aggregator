48da7a080a43f7c3afa4827a4b3a5433082620a1
Thamer Simon <thamersimon@gmail.com>
2024-03-07 20:15:34 +0100

    chore: updated example to newer version of runtime; spawning 1000 workers; also using stop command

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::time::Duration;
 use crossbeam_channel::unbounded;
-use generic_runtime::*;
 use generic_runtime::executor::Executor;
 use generic_runtime::handler::Handler;
-use generic_runtime::message::OutgoingMessage;
 use generic_runtime::module_runner::ModuleRunner;
-use log::info;
+use log::{info, warn};
 use rand::Rng;
 
+#[derive(Debug, Clone)]
 struct Message {
     name: String,
     data: u32
 }
 
+#[derive(Debug, Clone)]
 struct Worker {
     name: String
 }
@@ -26,29 +26,44 @@ impl Worker {
 }
 
 impl Executor<Message> for Worker{
-    fn execute(&self) -> Message {
+    fn execute(&self) -> Option<Message> {
         let mut rng = rand::thread_rng();
-        Message {
-            data: rng.gen_range(0..10),
+        Some(Message {
+            data: rng.gen_range(0..1000),
             name: self.name.clone()
-        }
+        })
+    }
+
+    fn on_stop(&mut self) {
+        warn!("Graceful shutdown of {}", self.name)
     }
 }
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init(); // enable logging of async
-    let (sender, receiver) = unbounded::<OutgoingMessage<Message>>();
+    let (sender, receiver) = unbounded::<Message>();
     let mut handler = Handler::new::<>();
-    let worker = Worker::new(String::from("Worker!"));
-    let runner = ModuleRunner::new(Box::new(worker), Duration::from_secs(3), sender);
-    handler.spawn(0, runner);
+    for i in 0..10000 {
+        // Spawn 1000 runners because why not :D
+        let worker = Worker::new(format!("Worker {:03}", i));
+        let duration = if i % 2 == 0 {
+            // Some of them should use a different delay
+            Duration::from_secs(3)
+        } else {
+            Duration::from_secs(5)
+        };
+        let runner = ModuleRunner::new(Box::new(worker), duration, sender.clone());
+        handler.spawn(i, runner);
+    }
 
+    let mut i = 0;
     receiver.iter().for_each(|message| {
-        match message {
-            OutgoingMessage::CollectedData(data) => {
-                info!("I am {}. The following Number has been generated: {}", data.name, data.data);
-            }
+        info!("I am {}. The following Number has been generated: {:03}", message.name, message.data);
+        if i % 100 == 0 {
+            // Kill next worker
+            handler.send_stop_message(i/100);
         }
+        i += 1;
     })
 }
